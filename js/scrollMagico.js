362bf6783a33ca5c4a9848d6f1ed2c014d7d7e91
walterfcr <wfallasb@ucenfotec.ac.cr>
2023-11-06 06:45:27 -0600

    menu responsive

--- js/scrollMagico.js
+++ js/scrollMagico.js
@@ -19,6 +19,7 @@ $(document).ready(function() {
 //desaparece el boton ScrollTop al cargar la pagina web
 document.getElementById("flechabtn").style.display = "none";
 
+
 //esconder botón ScrollTop (parte superior)
 window.onscroll = function() {
     scrollFunction()
@@ -26,10 +27,21 @@ window.onscroll = function() {
 
 //funcion scroll
 function scrollFunction() {
-    if (document.body.scrollTop > 280 || document.documentElement.scrollTop > 280) {
+    if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {
         document.getElementById("flechabtn").style.display = "block";
+        document.getElementById("logo").style.display = "none";
+        document.getElementById("container").style.backgroundColor = "#242222";
+        document.getElementById("container").style.width = "100%";
+        document.getElementById("listaMenu").style.textAlign = "center";
+
+
+
     } else {
         document.getElementById("flechabtn").style.display = "none";
+        document.getElementById("logo").style.display = "initial";
+        document.getElementById("container").style.backgroundColor = "initial";
+        document.getElementById("container").style.width = "90%";
+   
     }
 }
 
