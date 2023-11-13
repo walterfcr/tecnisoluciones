c2c3643b905e180ef8ad867f06df9e5f4043459a
walterfcr <wfallasb@ucenfotec.ac.cr>
2023-11-12 22:54:34 -0600

    arreglar menu

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,21 @@
-const toggleMenuElement = document.getElementById('toggle-menu');
-const mainMenuElement = document.getElementById('main-menu');
+var button = document.getElementById('hamburger-menu'),
+    span = button.getElementsByTagName('span')[0];
 
-toggleMenuElement.addEventListener('click', () => {
-    mainMenuElement.classList.toggle('main-menu-mostrar');
+button.onclick =  function() {
+  span.classList.toggle('hamburger-menu-button-close');
+};
+
+$('#hamburger-menu').on('click', toggleOnClass);
+
+function toggleOnClass(event) {
+  var toggleElementId = '#' + $(this).data('toggle'),
+  element = $(toggleElementId);
+
+  element.toggleClass('on');
+
+}
+
+// close hamburger menu after click a
+$( '.listaMenu li a' ).on("click", function(){
+  $('#hamburger-menu').click();
 });
